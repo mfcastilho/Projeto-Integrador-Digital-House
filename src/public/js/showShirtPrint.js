0728edf1e379812aad3c350cbee233ca544e7dcf
Mario Frederico Castilho <fredericocastilho@hotmail.com>
2022-12-15 01:53:31 -0300

    Preparing the html struct

--- src/public/js/showShirtPrint.js
+++ src/public/js/showShirtPrint.js
@@ -1,12 +1,33 @@
 
-const cardImage = document.querySelector(".product-card img");
 
+const productCards = document.querySelectorAll(".product-card");
+const tshirtsImage = document.querySelectorAll(".tshirt-image"); 
+const tshirtsPrint = document.querySelectorAll(".tshirt-print");
+
+//
 // const dataBase = require("../../data-base/dataBase.json");
 
-cardImage.addEventListener("mouseover", (event)=>{
-  cardImage.style.visibility = "hidden";
-});
 
-cardImage.addEventListener("mouseout", (event)=>{
-  cardImage.style.visibility = "visible";
-});
+// productCards.forEach((card)=>{
+//   card.addEventListener("mouseover", (event)=>{
+//     console.log("Passou");
+//   });
+  
+//   card.addEventListener("mouseleave", (event)=>{
+//     console.log("saiu"); 
+//   });
+// });
+
+for(let i = 0; i < productCards.length; i++){
+  productCards[i].addEventListener("mouseover", (event)=>{
+    console.log("Passou");
+    tshirtsImage[i].style.display = "none";
+    tshirtsPrint[i].style.display = "block";
+  });
+  
+  productCards[i].addEventListener("mouseleave", (event)=>{
+    console.log("saiu"); 
+    tshirtsPrint[i].style.display = "none";
+    tshirtsImage[i].style.display = "block";
+  });
+}
