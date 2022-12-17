db89c4033aea8559e31910914aec82461f8a2cc3
Mario Frederico Castilho <fredericocastilho@hotmail.com>
2022-12-17 02:22:20 -0300

    Fix validationRegisterForm.js

--- src/public/js/validationRegisterForm.js
+++ src/public/js/validationRegisterForm.js
@@ -14,6 +14,22 @@ window.addEventListener("load",()=>{
   const inputTel = document.querySelector("#tel-id");
   const inputGenre = document.querySelector("#genre-id");
   const inputBirthday = document.querySelector("#birthday-id");
+
+
+  const inputZipCode = document.querySelector("#zip-code-id");
+  const inputpublicPlace = document.querySelector("#public-place-id");
+  const inputNumber = document.querySelector("#number-id");
+  const inputComplement = document.querySelector("#complement-id");
+  const inputDistrict = document.querySelector("#district-id");
+  const inputReference = document.querySelector("#reference-id");
+  const inputCity = document.querySelector("#city-id");
+  const inputState = document.querySelector("#state-id");
+
+
+
+
+
+
   
   const button = document.querySelector("[data-create-account-button]");
 
