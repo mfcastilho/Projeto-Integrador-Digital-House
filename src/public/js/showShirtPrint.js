ce7d6283e29fb28231209e13f22d60cb979d03d5
Mario Frederico Castilho <fredericocastilho@hotmail.com>
2022-12-17 02:23:28 -0300

    Fix showShirPrint.js

--- src/public/js/showShirtPrint.js
+++ src/public/js/showShirtPrint.js
@@ -4,29 +4,19 @@ const productCards = document.querySelectorAll(".product-card");
 const tshirtsImage = document.querySelectorAll(".tshirt-image"); 
 const tshirtsPrint = document.querySelectorAll(".tshirt-print");
 
-//
-// const dataBase = require("../../data-base/dataBase.json");
-
-
-// productCards.forEach((card)=>{
-//   card.addEventListener("mouseover", (event)=>{
-//     console.log("Passou");
-//   });
-  
-//   card.addEventListener("mouseleave", (event)=>{
-//     console.log("saiu"); 
-//   });
-// });
 
 for(let i = 0; i < productCards.length; i++){
   productCards[i].addEventListener("mouseover", (event)=>{
-    console.log("Passou");
+    console.log("Passou por cima do card");
+
     tshirtsImage[i].style.display = "none";
     tshirtsPrint[i].style.display = "block";
+    
   });
   
   productCards[i].addEventListener("mouseleave", (event)=>{
-    console.log("saiu"); 
+    console.log("Saiu de cima do card"); 
+
     tshirtsPrint[i].style.display = "none";
     tshirtsImage[i].style.display = "block";
   });
