f5e33d70ce915d4f63fa7c3d7d4de251955e0862
mfcastilho <fredericocastilho@hotmail.com>
2023-03-04 23:07:02 -0300

    Fix user tel update infos

--- src/controller/UserController.js
+++ src/controller/UserController.js
@@ -22,10 +22,10 @@ const UserController = {
     return res.render("user-panel-personal-data.ejs", {user, birthday});
   },
   showEditUserPersonalDataPage: (req, res) =>{
-    const dbUsers = UserModel.findAll();
+    
     const {id} = req.params;
 
-    const user = dbUsers.findByPk(id);
+    const user = UserModel.findByPk(id);
     if(!user){
       return res 
         .status(404)
@@ -44,7 +44,7 @@ const UserController = {
            person, 
            name, 
            cpf, 
-           cel, 
+           tel, 
            birthday, 
           } = req.body;
 
@@ -53,6 +53,8 @@ const UserController = {
 
     const indexUser = dbUsers.findIndex(user=> user.id == id);
     const user = UserModel.findByPk(id);
+
+    console.log(tel);
 
     if(!user){
       return res 
@@ -67,8 +69,7 @@ const UserController = {
       person,
        name,
        cpf,
-       cel,
-       tel: user.tel,
+       tel,
        genre: user.genre,
        birthday,
        profilePicture: user.profilePicture,
@@ -81,7 +82,9 @@ const UserController = {
        city:user.city
     }
 
-    const response = UserModel.update(updateUser);
+    console.log()
+
+    const response = UserModel.update(id, updateUser);
 
     if(!response){
       return res 
@@ -162,96 +165,6 @@ const UserController = {
     }
 
     res.redirect(`/usuario/area-cliente/${id}/dados-pessoais`);  
-  },
-  showUserRequestesPage: (req, res)=>{
-    
-  },
-  showEditUserRequestes: (req, res)=>{
-    
-  },
-  updateUserRequestsInfos: (req, res)=>{
-
-    return res.redirect(`/usuario/area-cliente/${updateUser.id}/dados-pessoais`);
-  },
-  showUserAddressPage: (req, res)=>{
-    const {id} = req.params;
-    
-    const userFound = UserModel.findByPk(id);
-    if(!userFound){
-      res
-        .status(404)
-        .json("Usuário não encontrado");
-    }
-
-    const userAddress = userFound.address;
-    console.log(userAddress);
-  
-    return res.render("address-page.ejs", {address: userAddress, user:userFound});
-
-  },
-  showEditUserAddress: (req, res)=>{
-
-    const {id} = req.params;
-    
-    const userFound = UserModel.findByPk(id);
-    if(!userFound){
-      res
-        .status(404)
-        .json("Usuário não encontrado");
-    }
-
-    const userAddress = userFound.address;
-    console.log(userAddress);
-  
-    return res.render("address-page-edit.ejs", {address: userAddress, user:userFound});
-
-  },
-  updateUserAddressInfos: (req, res)=>{
-    
-    const {id} = req.params;
-    console.log(id)
-    const userFound = UserModel.findByPk(id);
-    
-    
-    console.log(req.body.publicPlace)
-
-        const userUpdate = {
-          id:userFound.id,
-          email:userFound.email,
-          password:userFound.password,
-          name:userFound.name,
-          cpf:userFound.cpf,
-          tel:userFound.tel,
-          genre:userFound.genre,
-          birthday:userFound.birthday,
-          profilePicture:userFound.profilePicture,
-          address:{
-            zipCode:req.body.zipCode,
-            publicPlace:req.body.publicPlace,
-            number:req.body.number,
-            complement:req.body.complement,
-            district:req.body.district,
-            reference:req.body.reference,
-            city:req.body.city,
-            state:req.body.state
-          }
-        }
-
-        const response = UserModel.update(id, userUpdate);
-
-        console.log(response)
-
-        if(!response){
-          res
-            .status(404)
-            .json("Erro!Usuário não foi atualizado no banco");
-        }
-
-        
-        res.redirect(`/usuario/area-cliente/${id}/dados-pessoais`);
-    
-   
-    
   },
   showUserRequestesPage: (req, res)=>{
     
