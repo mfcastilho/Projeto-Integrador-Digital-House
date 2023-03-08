805fa072b7c583259a1c78c5d90a0e83b0c4ffb9
mfcastilho <fredericocastilho@hotmail.com>
2023-03-08 14:10:31 -0300

    Creating Product Model

--- src/models/Product.js
+++ src/models/Product.js
@@ -1,32 +1,41 @@
-const { DataTypes } = require("sequelize");
-
 module.exports = (sequelize, DataTypes) =>{
-        const Product = sequelize.define("Product", {
-            id:{
-                type: DataTypes.STRING,
-                primaryKey: true,
-                allowNull: false
-            },
-            name:{
-                type: DataTypes.STRING,
-                allowNull: false
-            },
-            tshirt_print:{
-                type: DataTypes.STRING,
-                allowNull: false
-            },
-            price:{
-                type: DataTypes.DOUBLE,
-                allowNull: false
-            },
-            active:{
-                type: DataTypes.BOOLEAN
-            }
+    const Product = sequelize.define("Product", {
+        id:{
+            type: DataTypes.STRING,
+            primaryKey: true,
+            allowNull: false
         },
-        {
-            tableName: "products",
-            timestamps: true
-        });
+        name:{
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        tshirt_print:{
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        price:{
+            type: DataTypes.DOUBLE,
+            allowNull: false
+        },
+        active:{
+            type: DataTypes.BOOLEAN,
+            allowNull: false
+        },
+        category_id:{
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    },{
+        tableName: "products",
+        timestamps: true
+    });
 
-        return Product;
+    Product.associate = (models)=>{
+        Product.belongsTo(models.Category, {
+            foreignKey: "category_id",
+            as: "category"
+        });
     }
+
+    return Product;
+}
