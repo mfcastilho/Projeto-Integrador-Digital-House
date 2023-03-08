6a4178fce5ab72ab326227aaefd103e667d4c521
mfcastilho <fredericocastilho@hotmail.com>
2023-03-08 01:24:41 -0300

    Creating the files who we use to insert the datas in the database with seeders

---
+++ src/database/old/categoriesTableInfos.js
@@ -0,0 +1,18 @@
+
+const categories = [
+    {
+        id: "1",
+        category_name: "filme",
+        createdAt: new Date().toISOString(),
+        updatedAt: new Date().toISOString()
+    },
+    {
+        id: "2",
+        category_name: "animes",
+        createdAt: new Date().toISOString(),
+        updatedAt: new Date().toISOString()
+    }
+
+]
+
+module.exports = categories;
