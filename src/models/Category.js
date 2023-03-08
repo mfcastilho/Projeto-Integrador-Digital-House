1f41576641966b6eb81a14fc348047b9af1ab01b
mfcastilho <fredericocastilho@hotmail.com>
2023-03-08 14:11:20 -0300

    Creating Category Model

---
+++ src/models/Category.js
@@ -0,0 +1,18 @@
+module.exports = (sequelize, DataTypes)=>{
+    const Category = sequelize.define("Category", {
+        id:{
+            type: DataTypes.STRING,
+            primaryKey: true,
+            allowNull: false
+        },
+        category_name:{
+            type: DataTypes. STRING,
+            allowNull: false
+        }
+    },{
+        tableName:"categories",
+        timestamps: true
+    });
+
+    return Category;
+}
