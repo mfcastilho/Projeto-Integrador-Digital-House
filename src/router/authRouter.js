2797193c2a07c85b94027ffe00864f8913fd81d0
mfcastilho <fredericocastilho@hotmail.com>
2023-03-16 23:06:25 -0300

    feat: implementing user register validations

--- src/router/authRouter.js
+++ src/router/authRouter.js
@@ -2,16 +2,17 @@ const express = require("express");
 const router = express.Router();
 
 const loginFormValidationMiddleware = require("../middlewares/loginFormValidationMiddleware");
+const registerFormValidationMiddleware = require("../middlewares/registerFormValidationMiddleware")
 const AuthController = require("../controller/AuthController.js");
 
-const validations = loginFormValidationMiddleware;
-
+const loginValidation = loginFormValidationMiddleware;
+const registerValidations = registerFormValidationMiddleware
 
 router.get("/login", AuthController.showLogin);
-router.post("/login", validations, AuthController.logging);
+router.post("/login", loginValidation, AuthController.logging);
 
 router.get("/usuario/cadastro", AuthController.showRegister); 
-router.post("/usuario/cadastro", validations, AuthController.storeUser);
+router.post("/usuario/cadastro", registerValidations, AuthController.storeUser);
 
 router.post("/usuario/logout", AuthController.logout);
 
