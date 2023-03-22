ce464b5657b4d0be69b03e6469e7707b04323787
mfcastilho <fredericocastilho@hotmail.com>
2023-03-22 03:38:19 -0300

    feat: implementing showCheckout method and inserting the checkout infos in the view

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ const shoppingCartRouter = require("./router/shoppingCartRouter.js");
 const authRouter = require("./router/authRouter");
 const userRouter = require("./router/userRouter.js");
 const adminRouter = require("./router/adminRouter");
-const checkoutRouter = require("./router/checkoutRouter");
+const checkoutRouter = require("./router/checkoutRouter")
 
 
 
@@ -53,10 +53,11 @@ app.use(express.static(path.resolve("src", "public")));
 //Rotas
 app.use(homeRouter);
 app.use(shoppingCartRouter);
+app.use(checkoutRouter);
 app.use(authRouter);
 app.use(userRouter);
 app.use(adminRouter);
-app.use(checkoutRouter);
+
 
 
 
