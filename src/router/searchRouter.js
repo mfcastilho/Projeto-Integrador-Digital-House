73b060aabe1eb8ce38e187c94724a5dc06586534
mfcastilho <fredericocastilho@hotmail.com>
2023-03-25 03:45:00 -0300

    cleaning

--- src/router/searchRouter.js
+++ src/router/searchRouter.js
@@ -2,9 +2,12 @@ const express = require("express");
 const router = express.Router();
 const SearchController = require("../controller/SearchController");
 
+const errorMiddleware = require("../middlewares/genericErrorMiddleware");
+
 
 router.get("/search", SearchController.getSearch);
 
 
+router.use(errorMiddleware);
 
 module.exports = router;
